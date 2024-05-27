6489351c3185b541f707b1110e4bcc8193e93d90
Vic K <kondratskayavictoria@gmail.com>
2024-05-28 00:15:29 +0300

    finished developing memory addition

--- boxes/models.py
+++ boxes/models.py
@@ -1,15 +1,23 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
 
 class Box(models.Model):
+    class Statuses(models.TextChoices):
+        EDITED = 'E', _('Edited')
+        LOCKED = 'L', _('Locked')
+        OPENED = 'O', _('Opened')
+
     name = models.CharField(max_length=100)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_created = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(default=timezone.now)
     date_opening = models.DateTimeField()
+    status = models.CharField(choices=Statuses.choices, default=Statuses.EDITED, max_length=100)
 
     def __str__(self):
         return self.name
@@ -27,6 +35,11 @@ class Memory(models.Model):
     box = models.ForeignKey(Box, on_delete=models.CASCADE)
     content_type = models.CharField(choices=ContentType.choices, max_length=1, default=ContentType.TEXT)
     description = models.TextField()
+    text_content = models.TextField(blank=True, null=True)
+    image_content = models.ImageField(upload_to='images/', blank=True, null=True)
+    video_content = models.FileField(upload_to='videos/', blank=True, null=True)
+    audio_content = models.FileField(upload_to='audios/', blank=True, null=True)
+    file_content = models.FileField(upload_to='files/', blank=True, null=True)
 
     def __str__(self):
         return f"{self.name}({self.content_type})"
--- boxes/urls.py
+++ boxes/urls.py
@@ -1,7 +1,13 @@
+from django.conf.urls.static import static
 from django.urls import path
 import boxes.views
+from SYIAB import settings
 
 urlpatterns = [
     path('create/', boxes.views.create_a_box, name='create'),
-    path('boxes/<int:id>', boxes.views.view_box, name='view'),
-]
+    path('boxes/<int:box_id>', boxes.views.view_box, name='view_box'),
+    path('boxes/<int:box_id>/create_memory/', boxes.views.create_memory, name='create_memory'),
+    path('boxes/<int:box_id>/<int:memory_id>/', boxes.views.view_memory, name='view_memory'),
+    path('boxes/<int:box_id>/delete/', boxes.views.delete_box, name='delete_box'),
+    path('boxes/<int:box_id>/<int:memory_id>/delete/', boxes.views.delete_memory, name='delete_memory'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
