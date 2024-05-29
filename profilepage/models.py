cd7be487f4d770ad1512755ea0bbed2a3f3afc66
Vic K <kondratskayavictoria@gmail.com>
2024-05-29 12:03:44 +0300

    added profile app

---
+++ profilepage/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+
+class ProfilePicture(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pictures', default='default_profile_picture.png')
+
+    def __str__(self):
+        return str(self.user)
